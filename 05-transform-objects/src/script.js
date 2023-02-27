3c90fceca4bedc159e49f22246fa9ddd6085bfa5
Aphrx <aphrx@aphrx.ca>
2023-02-26 23:01:28 -0500

    Add lessons

---
+++ 05-transform-objects/src/script.js
@@ -0,0 +1,70 @@
+import * as THREE from 'three'
+import { TetrahedronGeometry } from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({color: '#ff0000'})
+)
+
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({color: '#00ff00'})
+)
+cube2.position.x = -2
+
+group.add(cube1)
+group.add(cube2)
+
+// Axes helper
+
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+
+
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+// Animations 
+
+const tick = () => {
+    console.log('tick')
+}
+
+tick()
